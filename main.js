f3c5ef161ea930463101573ffd53cf3b74d71857
Brad Chapin <paledoor@yahoo.com>
2017-08-25 11:01:32 -0700

    added display message for bad zipcode, still debugging display toggle

--- main.js
+++ main.js
@@ -1,12 +1,21 @@
 $(document).ready(function (){
-  var fiveDay = $("#fiveday");
-  var zip = $("#zip");
+
+  // getting html elements and assigning variables to them
+  var fiveDay = $("#fiveday"), zip = $("#zip"), err = $("#err");
+
+  // defining click event for "get weather" button
   $("#btn").on("click", function (){
     event.preventDefault();
+    err.hide();
     var $xhr = $.getJSON("api.openweathermap.org/data/2.5/forecast?zip=" + zip.val + "&format = json");
+    // error checking for bad/no zipcode
     if ((zip.val() == "") || (zip.val().length < 7)) {
-      alert("Please enter a valid zip code.");
-      console.log(zip.val());
+      err.show();
+      console.log();
+    } else {
+      $xhr.done(function (data){
+
+      });
     }
   });
 });
