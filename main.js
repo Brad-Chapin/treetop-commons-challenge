3d7452f89c31e7b7a8196bfecfeb78ba79771c78
Brad Chapin <paledoor@yahoo.com>
2017-08-25 10:44:25 -0700

    added CNAME, pushing to surge to test cors issue

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
 $(document).ready(function (){
-  console.log("working");
+  var fiveDay = $("#fiveday");
+  var zip = $("#zip");
+  $("#btn").on("click", function (){
+    event.preventDefault();
+    var $xhr = $.getJSON("api.openweathermap.org/data/2.5/forecast?zip=" + zip.val + "&format = json");
+    if ((zip.val() == "") || (zip.val().length < 7)) {
+      alert("Please enter a valid zip code.");
+      console.log(zip.val());
+    }
+  });
 });
