91671e78cc5f84f355af44f4a375cfda5d3c1b3e
Brad Chapin <paledoor@yahoo.com>
2017-08-26 09:58:09 -0700

    added app id from API

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ $(document).ready(function (){
   $("#btn").on("click", function (){
     event.preventDefault();
     err.hide();
-    var $xhr = $.getJSON("api.openweathermap.org/data/2.5/forecast?zip=" + zip.val + "&format = json");
+    var $xhr = $.getJSON("https://api.openweathermap.org/data/2.5/forecast?zip=" + zip.val() + "&APPID=286fc55d927f2ff688bc60806f52f979");
     // error checking for bad/no zipcode
     if ((zip.val() == "") || (zip.val().length < 7)) {
       err.show();
