e10b6e5b6dc2909c72654937b8840c2bcb61a04e
Brad Chapin <paledoor@yahoo.com>
2017-08-26 10:11:12 -0700

    zipcode validation working, api accepting request, still working on incoming data

--- main.js
+++ main.js
@@ -6,15 +6,16 @@ $(document).ready(function (){
   // defining click event for "get weather" button
   $("#btn").on("click", function (){
     event.preventDefault();
-    err.hide();
-    var $xhr = $.getJSON("https://api.openweathermap.org/data/2.5/forecast?zip=" + zip.val() + "&APPID=286fc55d927f2ff688bc60806f52f979");
+    $("#err").addClass("hide");
+    var $xhr = $.getJSON("https://api.openweathermap.org/data/2.5/forecast?zip=" + zip.val() + "&format=json" + "&APPID=286fc55d927f2ff688bc60806f52f979");
     // error checking for bad/no zipcode
-    if ((zip.val() == "") || (zip.val().length < 7)) {
-      err.show();
+    if ((zip.val() == "") || (zip.val().length != 5)) {
+      $("#err").removeClass("hide");
       console.log();
     } else {
+      $("#err").addClass("hide");
       $xhr.done(function (data){
-
+        console.log(data);
       });
     }
   });
